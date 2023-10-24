40475e81a0eb4221d3078b00493f412795d62220
artur24814 <artur24814@gmail.com>
2023-10-24 15:48:52 +0200

    endpoint fo get currencies has been created

--- app/currency_exchange/urls.py
+++ app/currency_exchange/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .api_views import (exchange_view)
+from .api_views import (exchange_view, ListCurrencies)
 
 app_name = 'currency_exchange'
 urlpatterns = [
-    path('<str:sell>/<str:buys>/', exchange_view, name='exchange-view')
+    path('', ListCurrencies.as_view(), name='list-currencies'),
+    path('<str:sell>/<str:buys>/', exchange_view, name='exchange-view'),
 ]
